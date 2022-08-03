ddf3c9407759a13be15f90e01784c5a7c201051d
Ralph Gasser <rg@pontius.ch>
2022-08-03 18:59:18 +0200

    Continued work on evaluation.

---
+++ evaluation/scripts/analytics-cottontaildb-cost.jl
@@ -0,0 +1,122 @@
+using DataFrames
+using Statistics
+using Gadfly
+using Cairo
+using Fontconfig
+using Query
+using Statistics
+using Formatting
+using CSV
+
+include("./load-file.jl");
+include("./recall.jl");
+
+
+# Load data files
+plans = Dict([
+    (301251082736639032, "P1: ScanIndex[PQ,feature,L2]"), 
+    (183418504497291366, "P2: ScanEntity[feature]"),
+    (74604402737375334, "P3: ScanEntity[id,feature]"), 
+    
+    # Range
+    (4817430002262786335, "P1: ScanIndex[PQ,feature,L2]"), 
+    (-2208122050355648033, "P2: ScanIndex[PQ,feature,L2]"),
+    (4817477855857574525, "P3: ScanIndex[PQ,feature,L2]"), 
+    (8744637559034823793, "P4: ScanEntity[feature]"), 
+    (-8427985728988109811, "P5: ScanEntity[feature]"), 
+    (1486491801562255110, "P6: ScanEntity[id,feature]"),
+
+
+    # NNS
+    (-4320830370914670928, "P1: ScanIndex[PQ,feature,L2]"), 
+    (4824833675157418254, "P2: ScanIndex[PQ,feature,L2]"),
+    (5729011964148328198, "P3: ScanIndex[VAF,feature,L2]"), 
+    (-4879747946231347746, "P4: ScanEntity[feature]"), 
+    (8752041231929455712, "P5: ScanEntity[feature]")
+])
+
+theme1 = Theme(
+    major_label_font="Helvetica Neue Bold",
+    major_label_font_size=16pt, 
+    minor_label_font="Helvetica Neue",
+    minor_label_font_size=12pt, 
+    key_title_font="Helvetica Neue Bold",
+    key_title_font_size=14pt,
+    key_label_font="Helvetica Neue",
+    key_label_font_size=12pt, 
+    point_label_font="Helvetica Neue Light",
+    point_label_font_size=18pt,
+    line_style=[:dot],
+    key_position=:none
+)
+
+theme2 = Theme(
+    major_label_font="Helvetica Neue Bold",
+    major_label_font_size=16pt, 
+    minor_label_font="Helvetica Neue",
+    minor_label_font_size=12pt, 
+    key_title_font="Helvetica Neue Bold",
+    key_title_font_size=14pt,
+    key_label_font="Helvetica Neue",
+    key_label_font_size=12pt, 
+    point_label_font="Helvetica Neue Light",
+    point_label_font_size=18pt,
+    point_size=6pt
+)
+
+
+df = DataFrame(Digest = Int64[],  Score = Float64[], Rank = Int32[], Plan = String[], Query = String[], Entity = String[], CPUW = Float64[], IOW = Float64[], QW = Float64[], PERF = Float64[])
+dict = read_json(joinpath("./evaluation/data/analytics/","cost-model.json"))
+for (digest, score, rank, plan, query, entity, cpu, io, quality) in zip(dict["digest"], dict["score"], dict["rank"], dict["plan"], dict["query"], dict["entity"], dict["cpu"], dict["io"], dict["quality"])
+    push!(df, (digest, score, rank, get(plans, digest, plan), query, entity, cpu, io, quality, cpu + io))
+end
+
+data1 = df |> @filter(_.Query == "Mean"  && _.CPUW == 0.3 && _.IOW  == 0.6 && _.Rank <= 3) |> DataFrame
+
+p1 = plot(data1, x=:QW, color = :Plan, y=:Score, Geom.smooth,
+    Scale.color_discrete_manual("#A5D7D2","#D20537","#2D373C","#D2EBE9","#DD879B","#46505A"),
+    Guide.xlabel("wQ"),
+    theme1
+)
+
+p2 = plot(data1, x=:QW, color = :Plan, y=:Rank, Geom.point,
+    Scale.color_discrete_manual("#A5D7D2","#D20537","#2D373C","#D2EBE9","#DD879B","#46505A"),
+    Guide.xlabel("wQ"),
+    Scale.y_discrete,
+    theme2
+)
+
+
+data2= df |> @filter(_.Query == "Range" && _.CPUW == 0.3 && _.IOW  == 0.6 && _.Rank <= 3) |> DataFrame
+
+p3 = plot(data2, x=:QW, color = :Plan, y=:Score, Geom.line,
+    Scale.color_discrete_manual("#A5D7D2","#D20537","#2D373C","#D2EBE9","#DD879B","#46505A"),
+    Guide.xlabel("wQ"),
+    theme1
+)
+
+p4 = plot(data2, x=:QW, color = :Plan, y=:Rank, Geom.point,
+    Scale.color_discrete_manual("#A5D7D2","#D20537","#2D373C","#D2EBE9","#DD879B","#46505A"),
+    Guide.xlabel("wQ"),
+    Scale.y_discrete,
+    theme2
+)
+
+data3 = df |> @filter(_.Query == "NNS" && _.CPUW == 0.3 && _.IOW  == 0.6 && _.Rank <= 3) |> DataFrame
+
+p5 = plot(data3, x=:QW, color = :Plan, y=:Score, Geom.line,
+    Scale.color_discrete_manual("#A5D7D2","#D20537","#2D373C","#D2EBE9","#DD879B","#46505A"),
+    Guide.xlabel("wQ"),
+    theme1
+)
+
+p6 = plot(data3, x=:QW, color = :Plan, y=:Rank, Geom.point,
+    Scale.color_discrete_manual("#A5D7D2","#D20537","#2D373C","#D2EBE9","#DD879B","#46505A"),
+    Guide.xlabel("wQ"),
+    Scale.y_discrete,
+    theme2
+)
+
+draw(PDF("./mainmatter/08-evaluation/figures/analytics/analytics-cottontail-cost-mean.pdf",28cm,10cm),hstack(p1,p2));
+draw(PDF("./mainmatter/08-evaluation/figures/analytics/analytics-cottontail-cost-range.pdf",28cm,12cm),hstack(p3,p4));
+draw(PDF("./mainmatter/08-evaluation/figures/analytics/analytics-cottontail-cost-nns.pdf",28cm,11cm),hstack(p5,p6));
--- evaluation/scripts/analytics-cottontaildb.jl
+++ evaluation/scripts/analytics-cottontaildb.jl
@@ -1,4 +1,3 @@
-
 using DataFrames
 using Statistics
 using Gadfly
@@ -14,41 +13,105 @@ include("./recall.jl");
 
 
 # Load data files
-df = DataFrame(Query = String[], Entity = String[], Index = String[], Parallel = Int32[], Runtime = Float64[], Recall = Float64[], NDCG = Float64[])
-dict = read_json(joinpath("./evaluation/data/analytics/","analytics~measurements.json"))
+entities = Dict([("features_averagecolor", 3), ("features_visualtextcoembedding", 25), ("features_hogmf25k512", 512), ("features_inceptionresnetv2", 1536), ("features_conceptmasksade20k", 2048)])
+indexes = Dict([("SCAN", 1), ("VAF", 2), ("PQ", 3)])
+queries = Dict([("Fetch", 1), ("Mean", 2), ("Range", 3), ("NNS", 4), ("Select", 5)])
+
+# Load Non-SIMD data
+df1 = DataFrame(Entity = String[],  Dimension = Int32[], Query = String[], QueryOrder = Int32[], Index = String[], IndexOrder = Int32[], Parallel = Int32[], Runtime = Float64[], Recall = Float64[], NDCG = Float64[])
+dict = read_json(joinpath("./evaluation/data/analytics/","analytics-normal~measurements.json"))
 for (entity, query, index, parallel, runtime, recall, dcg) in zip(dict["entity"], dict["query"], dict["index"], dict["parallel"], dict["runtime"], dict["recall"], dict["ndcg"])
-    push!(df, (query, entity, index, parallel, runtime, recall, dcg))
+    push!(df1, (replace(entity,"features_" => ""), entities[entity], query, queries[query], index, indexes[index], parallel, runtime, recall, dcg))
 end
 
-#print(df)
+df2 = DataFrame(Entity = String[], Query = String[], Index = String[], Plan = String[])
+dict = read_json(joinpath("./evaluation/data/analytics/","analytics-normal~plans.json"))
+for (entity, query, index, plan) in zip(dict["entity"], dict["query"], dict["index"], dict["plan"])
+    push!(df2, (entity, query, index, last(plan)))
+end
+
+df2 = df2 |> @filter(_.Query in ["Mean", "Range", "NNS"]) |> DataFrame
+
+# Load SIMD data
+df3 = DataFrame(Entity = String[],  Dimension = Int32[], Query = String[], QueryOrder = Int32[], Index = String[], IndexOrder = Int32[], Parallel = Int32[], Runtime = Float64[], Recall = Float64[], NDCG = Float64[])
+dict = read_json(joinpath("./evaluation/data/analytics/","analytics-simd~measurements.json"))
+for (entity, query, index, parallel, runtime, recall, dcg) in zip(dict["entity"], dict["query"], dict["index"], dict["parallel"], dict["runtime"], dict["recall"], dict["ndcg"])
+    push!(df3, (replace(entity,"features_" => ""), entities[entity], query, queries[query], index, indexes[index], parallel, runtime, recall, dcg))
+end
 
 # Generate PDFs
-nns = df |>
-@orderby(_.Entity) |> 
-@thenby(_.Index) |>
-@groupby({_.Entity, _.Query, _.Index, _.Parallel}) |> 
-@map({
-    Entity=key(_).Entity, 
-    Query=key(_).Query,
-    Index=key(_).Index,
-    Parallel=key(_).Parallel,
-    Type="$(key(_).Index) (p=$(key(_).Parallel))",
-    RuntimeMean=Statistics.mean(_.Runtime), 
-    RecallMean=Statistics.mean(_.Recall), 
-    NDCGMean=Statistics.mean(_.NDCG),
-    LabelPosition=minimum([Statistics.mean(_.Runtime), 100]), 
-}) |> DataFrame
+nnsr = df1 |>
+    @filter(_.Parallel in [2, 8, 16]) |>
+    @orderby_descending(_.Dimension) |> 
+    @thenby_descending(_.IndexOrder) |>
+    @thenby_descending(_.Parallel) |>
+    @thenby_descending(_.QueryOrder) |>
+    @groupby({_.Dimension, _.Query, _.Index, _.Parallel}) |> 
+    @map({
+        Dimension=key(_).Dimension, 
+        Query=key(_).Query,
+        Index=key(_).Index,
+        Parallel=key(_).Parallel,
+        Type="$(key(_).Index) (p=$(key(_).Parallel))",
+        RuntimeMean=Statistics.mean(_.Runtime)
+    }) |> DataFrame
 
-print(nns)
-
-p1 = plot(nns,
-    color=:Query, ygroup=:Entity, x=:RuntimeMean, y=:Type,
-    Geom.subplot_grid(layer(x=:LabelPosition, Geom.bar(orientation = :horizontal)), Guide.xticks(orientation=:vertical)),
+runtime_normal = plot(nnsr,
+    color=:Query, x=:RuntimeMean, y=:Type, ygroup=:Dimension,
+    Geom.subplot_grid(Geom.bar(orientation = :horizontal)),
     Guide.xlabel("Latency [s]"),
     Guide.ylabel(nothing),
-    Guide.colorkey(title="Query"),
-    Scale.x_continuous(minvalue = 0.0, maxvalue=20.0),
+    Guide.colorkey(title="Query", labels=["Fetch (Q1)","Mean (Q2)","Range (Q3)","NNS (Q4)","Select (Q5)"], pos=[0.8,-0.4]),
+    Scale.x_continuous(minvalue = 0.0, maxvalue=30.0),
     Scale.y_discrete,
+    Scale.color_discrete_manual("#A5D7D2","#D20537","#2D373C","#D2EBE9","#DD879B","#46505A"),
+    Theme(
+        major_label_font="Helvetica Neue Bold",
+        major_label_font_size=20pt, 
+        minor_label_font="Helvetica Neue",
+        minor_label_font_size=18pt, 
+        key_title_font="Helvetica Neue Bold",
+        key_title_font_size=20pt,
+        key_label_font="Helvetica Neue",
+        key_label_font_size=18pt, 
+        point_label_font="Helvetica Neue Light",
+        point_label_font_size=16pt, 
+        bar_spacing=1mm, 
+        default_color="#D2EBE9",
+        key_position=:inside
+    )
+)
+
+nnsq = df1 |>
+    @orderby(_.Dimension) |> 
+    @thenby_descending(_.IndexOrder) |>
+    @thenby_descending(_.Parallel) |>
+    @thenby_descending(_.QueryOrder) |>
+    @groupby({_.Dimension, _.Query, _.Index}) |> 
+    @map({
+        Dimension=key(_).Dimension, 
+        Query=key(_).Query,
+        Index=key(_).Index,
+        RecallMax=maximum(_.Recall),
+        RecallMean=Statistics.mean(_.Recall),
+        RecallMin=minimum(_.Recall), 
+        NDCGMax=maximum(_.NDCG), 
+        NDCGMean=Statistics.mean(_.NDCG),
+        NDCGMin=minimum(_.NDCG)
+    }) |> DataFrame
+
+quality_range = plot(nnsq |> @filter(_.Query == "Range") |> DataFrame,
+    x=:Index, xgroup=:Dimension, 
+    Geom.subplot_grid(
+        layer(y=:RecallMean, ymin=:RecallMin, ymax=:RecallMax, Geom.point, Geom.errorbar, color=["Recall"], shape = [Shape.diamond]),
+        layer(y=:NDCGMean, ymin=:NDCGMin, ymax=:NDCGMax, Geom.point, Geom.errorbar, color=["nDCG"], shape = [Shape.circle]),
+        Guide.xticks(orientation=:vertical),
+        Coord.cartesian(ymin=0.0, xmin=1.0)
+    ), 
+    Guide.xlabel(nothing),
+    Guide.ylabel("Quality"),
+    Scale.x_discrete,
+    Scale.color_discrete_manual("#D2EBE9","#DD879B"),
     Theme(
         major_label_font="Helvetica Neue Bold",
         major_label_font_size=20pt, 
@@ -56,9 +119,35 @@ p1 = plot(nns,
         minor_label_font_size=18pt, 
         point_label_font="Helvetica Neue Light",
         point_label_font_size=16pt, 
-        bar_spacing=1mm, 
+        point_size=6pt, 
         default_color="#D2EBE9"
     )
 )
 
-draw(PDF("./mainmatter/08-evaluation/figures/analytics/analytics-cottontail-runtime.pdf",24cm,48cm),p1);
+quality_nns = plot(nnsq |> @filter(_.Query == "NNS") |> DataFrame,
+    x=:Index, xgroup=:Dimension, y=:NDCGMean, ymin=:NDCGMin, ymax=:NDCGMax,
+    Geom.subplot_grid(
+        layer(y=:RecallMean, ymin=:RecallMin, ymax=:RecallMax, Geom.point, Geom.errorbar, color=["Recall"], shape = [Shape.diamond]),
+        layer(y=:NDCGMean, ymin=:NDCGMin, ymax=:NDCGMax, Geom.point, Geom.errorbar, color=["nDCG"], shape = [Shape.circle]),
+        Guide.xticks(orientation=:vertical),
+        Coord.cartesian(ymin=0.0, xmin=1.0)
+    ),
+    Guide.xlabel(),
+    Guide.ylabel("Quality"),
+    Scale.x_discrete,
+    Scale.color_discrete_manual("#D2EBE9","#DD879B"),
+    Theme(
+        major_label_font="Helvetica Neue Bold",
+        major_label_font_size=20pt, 
+        minor_label_font="Helvetica Neue",
+        minor_label_font_size=18pt, 
+        point_label_font="Helvetica Neue Light",
+        point_label_font_size=16pt, 
+        point_size=6pt, 
+        default_color="#D2EBE9"
+    )
+)
+
+draw(PDF("./mainmatter/08-evaluation/figures/analytics/analytics-cottontail-runtime.pdf",36cm,49cm),runtime_normal);
+draw(PDF("./mainmatter/08-evaluation/figures/analytics/analytics-cottontail-quality-range.pdf",22cm,11cm),quality_range);
+draw(PDF("./mainmatter/08-evaluation/figures/analytics/analytics-cottontail-quality-nns.pdf",22cm,11cm),quality_nns);
