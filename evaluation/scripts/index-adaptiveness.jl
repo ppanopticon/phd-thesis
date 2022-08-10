0395df32c8b32cfeefde23fcd45f1ee67b8b6f3c
Ralph Gasser <rg@pontius.ch>
2022-08-10 09:41:22 +0200

    Fixed missing label.

--- evaluation/scripts/index-adaptiveness.jl
+++ evaluation/scripts/index-adaptiveness.jl
@@ -31,30 +31,34 @@ theme = Theme(
 entities = Dict([("features_averagecolor", 3), ("features_visualtextcoembedding", 25), ("features_hogmf25k512", 512), ("features_inceptionresnetv2", 1536), ("features_conceptmasksade20k", 2048)])
 indexes = Dict([("SCAN", 1), ("VAF", 2), ("PQ", 3)])
 
-df1 = DataFrame(Timestamp = Int32[], Count = Int32[], Insert = Int32[], Delete = Int32[], OOB = Int32[], Runtime = Float64[], NDCG = Float64[], Recall = Float64[])
+df1 = DataFrame(Timestamp = Int32[], Count = Int32[], Insert = Int32[], Delete = Int32[], OOB = Int32[], Rebuild = Bool[], Runtime = Float64[], NDCG = Float64[], Recall = Float64[])
 dict = read_json(joinpath("./evaluation/data/index/","index-vaf-adaptiveness-with-rebuild~measurements.json"))
-for (timestamp, count, insert, delete, oob, runtime, ndcg, recall) in zip(dict["timestamp"],dict["count"],dict["insert"], dict["delete"], dict["oob"], dict["runtime"], dict["dcg"], dict["recall"])
-    push!(df1, (timestamp, count, insert, delete, oob, runtime, ndcg, recall))
+for (timestamp, count, insert, delete, oob, rebuild, runtime, ndcg, recall) in zip(dict["timestamp"],dict["count"],dict["insert"], dict["delete"], dict["oob"], dict["rebuilt"], dict["runtime"], dict["dcg"], dict["recall"])
+    push!(df1, (timestamp, count, insert, delete, oob, rebuild, runtime, ndcg, recall))
 end
+rebuild_df1 = (df1 |> @filter(_.Rebuild == true) |> DataFrame)[1,:Timestamp]
 
-df2 = DataFrame(Timestamp = Int32[], Count = Int32[], Insert = Int32[], Delete = Int32[], Runtime = Float64[], NDCG = Float64[], Recall = Float64[])
+df2 = DataFrame(Timestamp = Int32[], Count = Int32[], Insert = Int32[], Delete = Int32[], Rebuild = Bool[], Runtime = Float64[], NDCG = Float64[], Recall = Float64[])
 dict = read_json(joinpath("./evaluation/data/index/","index-pq-adaptiveness-with-rebuild~measurements.json"))
-for (timestamp, count, insert, delete, runtime, ndcg, recall) in zip(dict["timestamp"],dict["count"],dict["insert"], dict["delete"], dict["runtime"], dict["dcg"], dict["recall"])
-    push!(df2, (timestamp, count, insert, delete, runtime, ndcg, recall))
+for (timestamp, count, insert, delete, rebuild, runtime, ndcg, recall) in zip(dict["timestamp"],dict["count"],dict["insert"], dict["delete"], dict["rebuilt"], dict["runtime"], dict["dcg"], dict["recall"])
+    push!(df2, (timestamp, count, insert, delete, rebuild, runtime, ndcg, recall))
 end
+rebuild_df2 = (df2 |> @filter(_.Rebuild == true) |> DataFrame)[1,:Timestamp]
 
 # Prepare plot 
 count_vaf = plot(df1, x=:Timestamp,
     layer(y=:Count, Geom.line),
     Guide.xlabel("Ellapsed Time [s]"),
     Guide.ylabel("Collection Size"),
-    Coord.cartesian(xmin=0, xmax=3600),
+    layer(xintercept=[rebuild_df1], Geom.vline(color=["#46505A"], style=[:dot])),
+    Coord.cartesian(xmin=0, xmax=7200),
     theme
 )
 count_pq = plot(df2, x=:Timestamp,
     layer(y=:Count, Geom.line),
     Guide.xlabel("Ellapsed Time [s]"),
     Guide.ylabel("Collection Size"),
+    layer(xintercept=[rebuild_df2], Geom.vline(color=["#46505A"], style=[:dot])),
     Coord.cartesian(xmin=0, xmax=7200),
     theme
 )
@@ -63,6 +67,7 @@ operations_vaf = plot(df1, x=:Timestamp,
     layer(y=:Insert, Geom.line, color=["Inserts"]),
     layer(y=:Delete, Geom.line,  color=["Deletes"]),
     layer(y=:OOB, Geom.line,  color=["Tombstones"]),
+    layer(xintercept=[rebuild_df1], Geom.vline(color=["#46505A"], style=[:dot])),
     Guide.xlabel("Ellapsed Time [s]"),
     Guide.ylabel("Operations"),
     Scale.color_discrete_manual("#A5D7D2","#D20537"),
@@ -72,6 +77,7 @@ operations_vaf = plot(df1, x=:Timestamp,
 operations_pq = plot(df2, x=:Timestamp,
     layer(y=:Insert, Geom.line, color=["Inserts"]),
     layer(y=:Delete, Geom.line,  color=["Deletes"]),
+    layer(xintercept=[rebuild_df2], Geom.vline(color=["#46505A"], style=[:dot])),
     Guide.xlabel("Ellapsed Time [s]"),
     Guide.ylabel("Operations"),
     Scale.color_discrete_manual("#A5D7D2","#D20537"),
@@ -80,16 +86,16 @@ operations_pq = plot(df2, x=:Timestamp,
 )
 
 runtime_vaf = plot(df1, x=:Timestamp, y=:Runtime,
-    layer(Geom.smooth, Theme(default_color="#D20537")),
-    layer(Geom.line),
+    layer(Geom.line, Stat.smooth(method=:lm, levels=[0.70,0.80,0.90,0.99]), Geom.line, Geom.ribbon(fill=true)),
+    layer(xintercept=[rebuild_df1], Geom.vline(color=["#46505A"], style=[:dot])),
     Guide.xlabel("Ellapsed Time [s]"),
     Guide.ylabel("Latency [s]"),
     Coord.cartesian(xmin=0, xmax=7200),
     theme
 )
 runtime_pq = plot(df2, x=:Timestamp, y=:Runtime,
-    layer(Geom.smooth, Theme(default_color="#D20537")),
-    layer(Geom.line),
+    layer(Geom.line,  Stat.smooth(method=:lm, levels=[0.70,0.80,0.90,0.99]), Geom.line, Geom.ribbon(fill=true)),
+    layer(xintercept=[rebuild_df2], Geom.vline(color=["#46505A"], style=[:dot])),
     Guide.xlabel("Ellapsed Time [s]"),
     Guide.ylabel("Latency [s]"),
     Coord.cartesian(xmin=0, xmax=7200),
@@ -99,6 +105,7 @@ runtime_pq = plot(df2, x=:Timestamp, y=:Runtime,
 quality_vaf = plot(df1, x=:Timestamp, 
     layer(y=:NDCG, color=["nDCG"], Geom.line),
     layer(y=:Recall, color=["Recall"], Geom.line),
+    layer(xintercept=[rebuild_df1], Geom.vline(color=["#46505A"], style=[:dot])),
     Guide.xlabel("Ellapsed Time [s]"),
     Guide.ylabel("Quality"),
     Scale.y_continuous(minvalue=0.0, maxvalue=1.0),
@@ -107,8 +114,9 @@ quality_vaf = plot(df1, x=:Timestamp,
     theme
 )
 quality_pq = plot(df2, x=:Timestamp, 
-    layer(y=:NDCG, color=["nDCG"], Geom.line),
-    layer(y=:Recall, color=["Recall"], Geom.line),
+    layer(y=:NDCG, color=["nDCG"], Stat.smooth(method=:lm, levels=[0.9,0.99]), Geom.line, Geom.ribbon(fill=true)),
+    layer(y=:Recall, color=["Recall"], Stat.smooth(method=:lm, levels=[0.9,0.999]), Geom.line, Geom.ribbon(fill=true)),
+    layer(xintercept=[rebuild_df2], Geom.vline(color=["#46505A"], style=[:dot])),
     Guide.xlabel("Ellapsed Time [s]"),
     Guide.ylabel("Quality"),
     Scale.y_continuous(minvalue=0.0, maxvalue=1.0),
